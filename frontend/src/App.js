04a5beea19c173afcae3cc10526329a47237c5ef
Nandhabalan Marimuthu <67896802+nandha-marimuthu@users.noreply.github.com>
2020-10-22 14:09:46 +0530

    components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.png';
-import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Vugha Technologies
-        </p>
-        <a
-          className="App-link"
-          href="https://vugha.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Go to vugha
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+    <main><h1> welcome to vugha</h1>
+    <a href="https://vugha.com" target='_blank' rel="noopener noreferrer">go to vugha</a>
+    </main>
+    <Footer />
+    </>
   );
 }
 
