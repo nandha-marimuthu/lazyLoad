8db8def6683cf05d0cdaca320268699705a88303
Monisa99 <70888183+Monisa99@users.noreply.github.com>
2020-10-27 18:20:44 +0530

    changes 1

---
+++ frontend/src/reducers/productsReducer.js
@@ -0,0 +1,22 @@
+const PRODUCT_LIST_REQUEST = 'PRODUCT_LIST_REQUEST'
+const PRODUCTS_LIST_SUCCESS = 'PRODUCTS_LIST_SUCCESS'
+const PRODUCT_LIST_FAIL = 'PRODUCT_LIST_FAIL'
+
+export const productListReducer = (state = { products:[] } , action) => {
+  
+  switch(action.type){
+
+    case PRODUCT_LIST_REQUEST:
+      return { loading: true}
+
+    case PRODUCTS_LIST_SUCCESS:
+      return { loading:false, products: action.payload}
+
+    case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload }
+
+    default:
+      return state
+
+  }
+}
