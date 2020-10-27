b73b553f275e1151de0bf6485f671d4edeb432af
Monisa99 <70888183+Monisa99@users.noreply.github.com>
2020-10-27 11:21:50 +0530

    product description page added along with nodemon and concurrently

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import {Navbar,Nav,Container} from 'react-bootstrap'
 
 const Header = () =>{
@@ -6,20 +7,18 @@ const Header = () =>{
     <header>
       <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
         <Container>
-  <Navbar.Brand href="#home">Vugha</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="ml-auto">
-      <Nav.Link href="#home active">Cart</Nav.Link>
-      <Nav.Link href="#link">Signin</Nav.Link>
-      
-    </Nav>
-    
-  
-  </Navbar.Collapse>
-  </Container>
-</Navbar>
-      
+          <Navbar.Brand href="#home">Vugha</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link><Link to ="/">Home</Link></Nav.Link>
+              <Nav.Link><Link to ="/cart">Cart</Link></Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        
+        </Container>
+      </Navbar>
     </header>
   )
 }
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,31 +1,29 @@
-import React from 'react'
-import {Card} from 'react-bootstrap'
+import React from "react";
+import { Card } from "react-bootstrap";
 
-function Product({product}) {
+function Product({ product }) {
   return (
-    <Card className='my-3 p-3 rounded'>
-      <a href={`/products/${product._id}`}>
-      <Card.Img src={product.image} variant='top'/>
+    <Card className="my-3 p-3 rounded">
+      <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
       </a>
+
       <Card.Body>
-      <a href={`/products/${product._id}`}>
-      <Card.Title as='div' >
-        <strong>{product.name}</strong>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
 
-      </Card.Title>
-      </a>
-      <Card.Text as="div">
-        <div className='my-3'>
-          {product.rating} from {product.numReviews} Reviews
-
-        </div>
-
-      </Card.Text>
-      <Card.Text as="h3">${product.price}</Card.Text>
-
+        <Card.Text as="div">
+          <div className="my-3">
+            {product.rating} from {product.numReviews} Reviews
+          </div>
+        </Card.Text>
+        <Card.Text as="h3">&#x20B9; {product.price}</Card.Text>
       </Card.Body>
     </Card>
-  )
+  );
 }
 
-export default Product
+export default Product;
